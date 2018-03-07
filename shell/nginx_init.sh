fd08c3e12c9768674bf591b0e135bae52d5b54af
jaylu163 <320211697@qq.com>
2018-03-07 11:46:47 +0800

    nginx官方脚本
    nginx官方脚本
    https://www.nginx.com/resources/wiki/start/topics/examples/redhatnginxinit/

--- shell/nginx_init.sh
+++ shell/nginx_init.sh
@@ -6,37 +6,48 @@
 # description:  NGINX is an HTTP(S) server, HTTP(S) reverse \
 #               proxy and IMAP/POP3 proxy server
 # processname: nginx
-# config:      /usr/local/nginx/nginx.conf
+# config:      /etc/nginx/nginx.conf
 # config:      /etc/sysconfig/nginx
 # pidfile:     /var/run/nginx.pid
+
 # Source function library.
 . /etc/rc.d/init.d/functions
+
 # Source networking configuration.
 . /etc/sysconfig/network
+
 # Check that networking is up.
 [ "$NETWORKING" = "no" ] && exit 0
-nginx="/usr/local/nginx/sbin/nginx"
+
+nginx="/usr/sbin/nginx"
 prog=$(basename $nginx)
-NGINX_CONF_FILE="/usr/local/nginx/conf/nginx.conf"
+
+NGINX_CONF_FILE="/etc/nginx/nginx.conf"
+
 [ -f /etc/sysconfig/nginx ] && . /etc/sysconfig/nginx
+
 lockfile=/var/lock/subsys/nginx
+
 make_dirs() {
    # make required directories
-   user=`$nginx -V 2>&1 | grep "configure arguments:" | sed 's/[^*]*--user=\([^ ]*\).*/\1/g' -`
-   if [ -z "`grep $user /etc/passwd`" ]; then
-       useradd -M -s /bin/nologin $user
-   fi
-   options=`$nginx -V 2>&1 | grep 'configure arguments:'`
-   for opt in $options; do
-       if [ `echo $opt | grep '.*-temp-path'` ]; then
-           value=`echo $opt | cut -d "=" -f 2`
-           if [ ! -d "$value" ]; then
-               # echo "creating" $value
-               mkdir -p $value && chown -R $user $value
-           fi
-       fi
-   done
+   user=`$nginx -V 2>&1 | grep "configure arguments:.*--user=" | sed 's/[^*]*--user=\([^ ]*\).*/\1/g' -`
+   if [ -n "$user" ]; then
+      if [ -z "`grep $user /etc/passwd`" ]; then
+         useradd -M -s /bin/nologin $user
+      fi
+      options=`$nginx -V 2>&1 | grep 'configure arguments:'`
+      for opt in $options; do
+          if [ `echo $opt | grep '.*-temp-path'` ]; then
+              value=`echo $opt | cut -d "=" -f 2`
+              if [ ! -d "$value" ]; then
+                  # echo "creating" $value
+                  mkdir -p $value && chown -R $user $value
+              fi
+          fi
+       done
+    fi
 }
+
 start() {
     [ -x $nginx ] || exit 5
     [ -f $NGINX_CONF_FILE ] || exit 6
@@ -48,6 +59,7 @@ start() {
     [ $retval -eq 0 ] && touch $lockfile
     return $retval
 }
+
 stop() {
     echo -n $"Stopping $prog: "
     killproc $prog -QUIT
@@ -56,12 +68,14 @@ stop() {
     [ $retval -eq 0 ] && rm -f $lockfile
     return $retval
 }
+
 restart() {
     configtest || return $?
     stop
     sleep 1
     start
 }
+
 reload() {
     configtest || return $?
     echo -n $"Reloading $prog: "
@@ -69,18 +83,23 @@ reload() {
     RETVAL=$?
     echo
 }
+
 force_reload() {
     restart
 }
+
 configtest() {
   $nginx -t -c $NGINX_CONF_FILE
 }
+
 rh_status() {
     status $prog
 }
+
 rh_status_q() {
     rh_status >/dev/null 2>&1
 }
+
 case "$1" in
     start)
         rh_status_q && exit 0
